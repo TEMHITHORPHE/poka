2eb15b19d8a8ea4b29eb41f2d563b4480caec5e3
TEMHITHORPHE <tehmhithorphe@gmail.com>
2025-01-31 03:05:13 +0100

    update: added "flush" logic eval.

--- src/core/engine.rs
+++ src/core/engine.rs
@@ -27,12 +27,12 @@ impl PokaEngine {
 
     fn texas_hold_em<'a>(players_hands: &'a [&'a Hand]) -> Vec<HandEvaluationResult<'a>> {
         let mut hand_ranks = Vec::<HandEvaluationResult>::with_capacity(players_hands.len());
-        print!("Player Hands: {} {}", players_hands.len(), hand_ranks.len());
+        println!("Player Hands: {}", players_hands.len());
         let mut hand_rank_freq = [[0_u8, 0_u8], [0, 0], [0, 0], [0, 0], [0, 0]];
 
         // need a sorted player hand to make certain logic evaluation easier.
-        let mut sorted_players_hand: [Rank; 5] = [Rank::None, Rank::None, Rank::None, Rank::None, Rank::None];
-        print!("Sorted Players Hands: {}", sorted_players_hand.len());
+        let mut sorted_players_hand: [u8; 5] = [0_u8, 0_u8, 0_u8, 0_u8, 0_u8];
+        println!("Sorted Players Hands: {}", sorted_players_hand.len());
 
         for (hand_idx, hand) in players_hands.iter().enumerate() {
             println!("\n==== Card {} ====", hand_idx + 1);
@@ -40,11 +40,13 @@ impl PokaEngine {
             // ================= ==== ====== Weird Heap Free Implementation ===== ===== ===================
             // preliminary ops
             //
-            sorted_players_hand[0] = hand[0].rank;
-            sorted_players_hand[1] = hand[1].rank;
-            sorted_players_hand[2] = hand[2].rank;
-            sorted_players_hand[3] = hand[3].rank;
-            sorted_players_hand[4] = hand[4].rank;
+            sorted_players_hand[0] = hand[0].rank as u8;
+            sorted_players_hand[1] = hand[1].rank as u8;
+            sorted_players_hand[2] = hand[2].rank as u8;
+            sorted_players_hand[3] = hand[3].rank as u8;
+            sorted_players_hand[4] = hand[4].rank as u8;
+
+            sorted_players_hand.sort_by(|rank_a, rank_b| rank_a.cmp(rank_b));
             // structure below is just me avoiding to use an hashmap or similar construct (heap avoidance).
             // [ (Rank, Frequency) ]
             let mut j = 0_usize;
@@ -105,12 +107,21 @@ impl PokaEngine {
             //
             //
             // [EVALUATE]: Straight
-            // if hand_rank_freq[0][1] == hand_rank_freq[1][1] == 2_u8 {
-            //     hand_ranks.push((hand_idx as u8, hand, HandRank::Straight));
-            //     println!("|\n|=> {:?} ", HandRank::Straight);
-            // }
+            if (sorted_players_hand[0] + 1 == sorted_players_hand[1]
+                && sorted_players_hand[1] + 1 == sorted_players_hand[2]
+                && sorted_players_hand[2] + 1 == sorted_players_hand[3]
+                && sorted_players_hand[3] + 1 == sorted_players_hand[4])
+                && (hand[0].suit != hand[1].suit
+                    || hand[1].suit != hand[2].suit
+                    || hand[2].suit != hand[3].suit
+                    || hand[3].suit != hand[4].suit)
+            {
+                hand_ranks.push((hand_idx as u8, hand, HandRank::Straight));
+                println!("|\n|=> {:?} ", HandRank::Straight);
+            }
 
             println!("Card Freq: {:?}", hand_rank_freq);
+            println!("Card Sorted: {:?}", sorted_players_hand);
 
             // We reset our rank/frequency array.
             hand_rank_freq.fill([0_u8, 0_u8]);
@@ -119,8 +130,8 @@ impl PokaEngine {
         return hand_ranks;
 
         // ================= ==== ====== Weird Heap Free Implementation ===== ===== ===================
-        // 
-        // 
+        //
+        //
         // let mut cards_rank_weight = [
         //     (hand[0].rank as u8),
         //     (hand[1].rank as u8),

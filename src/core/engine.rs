252c8034eb586c279c303ed67c3c9574d5a02da3
TEMHITHORPHE <tehmhithorphe@gmail.com>
2025-03-30 14:49:52 +0100

    update: improve comments for hand rank frequency logic in texas_hold_em function

--- src/core/engine.rs
+++ src/core/engine.rs
@@ -28,6 +28,8 @@ impl PokaEngine {
     fn texas_hold_em<'a>(players_hands: &'a [&'a Hand]) -> Vec<HandEvaluationResult<'a>> {
         let mut hand_ranks = Vec::<HandEvaluationResult>::with_capacity(players_hands.len());
         println!("Player Hands: {}", players_hands.len());
+
+        // An array of array, [ [Rank, Frequency] ]
         let mut hand_rank_freq = [[0_u8, 0_u8], [0, 0], [0, 0], [0, 0], [0, 0]];
 
         // need a sorted player hand to make certain logic evaluation easier.
@@ -49,12 +51,12 @@ impl PokaEngine {
             sorted_players_hand.sort_by(|rank_a, rank_b| rank_a.cmp(rank_b));
 
             // structure below is just me avoiding to use an hashmap or similar construct (heap avoidance).
-            // [ (Rank, Frequency) ]
+            // code below counts the frequency of occurrence of a card in a particular hand.
+            // An array of [ [Rank, Frequency] ]
             let mut j = 0_usize;
             for card in hand.iter() {
                 let rank = card.rank as u8;
                 // sorted_players_hand[]
-                // code below counts the frequency of occurrence of a card in a particular hand
                 if rank == hand_rank_freq[0][0] {
                     hand_rank_freq[0] = [rank, hand_rank_freq[0][1] + 1];
                 } else if rank == hand_rank_freq[1][0] {
@@ -186,7 +188,7 @@ impl PokaEngine {
             println!("Card Freq: {:?}", hand_rank_freq);
             println!("Card Sorted: {:?}", sorted_players_hand);
 
-            // We reset our rank/frequency array.
+            // We reset our [rank, frequency] array.
             hand_rank_freq.fill([0_u8, 0_u8]);
         }
 
